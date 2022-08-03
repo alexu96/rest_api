345d38c38e44b9fdd5a277bf4e81842eeee03b50
Gulsan Borbhuiya <gulsanborbhuiya@Gulsans-MacBook-Pro.local>
2022-08-03 19:38:29 +0530

    Add DTOInitial

---
+++ HotelListing.API/Data/Country.cs
@@ -0,0 +1,21 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public int CountryId { get; set; }  
+
+
+
+        public virtual IList<Hotel> Hotels { get; set; }
+
+
+
+
+    }
+}
