c7d996b0d49efc8132dad253e025eb436a8beb0b
Gulsan Borbhuiya <gulsanborbhuiya@Gulsans-MacBook-Pro.local>
2022-08-03 19:38:29 +0530

    Initial

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public double Rating { get; set; }
+
+        [ForeignKey(nameof(CountryId))]
+
+        // This is foreign key
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+
+
+
+
+    }
+}
+
---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels{ get; set; }
+
+        public DbSet<Country> Countries { get; set;  }
+    }
+}
+
+[AttributeUsage(AttributeTargets.[System.AttributeUsage(AttributeTargets.[System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+public sealed class MyAttribute : Attribute
+{
+    readonly string positionalString;
+    public string PositionalString
+    {
+        get
+        {
+            return positionalString;
+        }
+    }
+
+    public int NamedInt { get; set; }
+
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+
+        // TODO: Implement code here
+
+        throw new NotImplementedException();
+    }
+}, Inherited = false, AllowMultiple = true)]
+public sealed class MyAttribute : Attribute
+{
+    readonly string positionalString;
+    public string PositionalString
+    {
+        get
+        {
+            return positionalString;
+        }
+    }
+
+    public int NamedInt { get; set; }
+
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+
+        // TODO: Implement code here
+
+        throw new NotImplementedException();
+    }
+}, Inherited = false, AllowMultiple = true)]
+public sealed class MyAttribute : Attribute
+{
+    readonly string positionalString;
+    public string PositionalString
+    {
+        get
+        {
+            return positionalString;
+        }
+    }
+
+    public int NamedInt { get; set; }
+
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+
+        // TODO: Implement code here
+
+        throw new NotImplementedException();
+    }
+}
